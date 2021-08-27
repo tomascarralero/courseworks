ca00193ea76dee13614f2c730fb42a03325e7a40
tomascarralero <tcarralero@gmail.com>
2021-08-28 00:44:58 +0200

    Added config and annotations for date timestamp autogenerated attributes at Usuario and Compra entities/models

---
+++ openwebinars/spring-boot/src/main/java/com/openwebinars/secondhandmarket/config/ConfiguracionAuditoria.java
@@ -0,0 +1,9 @@
+package com.openwebinars.secondhandmarket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class ConfiguracionAuditoria {
+}
