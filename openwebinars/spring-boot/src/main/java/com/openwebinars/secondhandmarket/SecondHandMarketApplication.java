fd1191f9e028c0416df637822ec1edba014d72a8
tomascarralero <tcarralero@gmail.com>
2021-08-27 23:10:00 +0200

    initial commit

---
+++ openwebinars/spring-boot/src/main/java/com/openwebinars/secondhandmarket/SecondHandMarketApplication.java
@@ -0,0 +1,13 @@
+package com.openwebinars.secondhandmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandMarketApplication.class, args);
+    }
+
+}
