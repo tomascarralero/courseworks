2410389c76e503e41ecc98c99b1d3b01617d7307
tomascarralero <tcarralero@gmail.com>
2021-08-29 22:51:41 +0200

    Implements user detail service and configure security authorization to use BCrypt encryption

--- openwebinars/spring-boot/src/main/java/com/openwebinars/secondhandmarket/seguridad/SeguridadConfig.java
+++ openwebinars/spring-boot/src/main/java/com/openwebinars/secondhandmarket/seguridad/SeguridadConfig.java
@@ -1,18 +1,30 @@
 package com.openwebinars.secondhandmarket.seguridad;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SeguridadConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
