b1ca6ada591693b9212b9c748ff430c9fa798d7d
tomascarralero <tcarralero@gmail.com>
2021-08-31 17:20:58 +0200

    Add @Service annotation for userDetailService

--- openwebinars/spring-boot/src/main/java/com/openwebinars/secondhandmarket/seguridad/UserDetailsServiceImpl.java
+++ openwebinars/spring-boot/src/main/java/com/openwebinars/secondhandmarket/seguridad/UserDetailsServiceImpl.java
@@ -8,7 +8,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service("userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
